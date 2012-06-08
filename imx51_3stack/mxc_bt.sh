6f2e62161ce057f4d741c9e049905cf03ed7e2a9
Xinyu Chen <xinyu.chen@freescale.com>
2012-06-08 13:44:41 +0800

    ENGR00000001 release branch created
    Move the proprietary dir into a new git.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- imx51_3stack/mxc_bt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-/system/xbin/bccmd -t bcsp -d /dev/ttymxc1 psload -r /system/etc/bluez/csr.psr;
-/system/bin/hciattach -n -s 921600 /dev/ttymxc1 bcsp 921600;
-
-exit 0;
---
+++ imx6/vendorsetup.sh
@@ -0,0 +1,11 @@
+
+add_lunch_combo imx6q_arm2-eng
+add_lunch_combo imx6q_arm2-user
+add_lunch_combo sabresd_6q-eng
+add_lunch_combo sabresd_6q-user
+add_lunch_combo sabresd_6dl-eng
+add_lunch_combo sabresd_6dl-user
+add_lunch_combo imx6q_sabreauto-eng
+add_lunch_combo imx6q_sabreauto-user
+add_lunch_combo imx6sl_arm2-eng
+add_lunch_combo imx6sl_arm2-user
