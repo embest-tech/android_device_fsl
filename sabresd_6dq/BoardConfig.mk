aeb278f1292944564a7cb39c6ad93ee29a20c11e
Zhang Jiejing <jiejing.zhang@freescale.com>
2012-06-21 11:00:45 +0800

    ENGR00000001 update saberSD 6DQ bootcmd for support lvds
    update bootcmd to support lvds only display.
    
    Signed-off-by: Zhang Jiejing <jiejing.zhang@freescale.com>

--- sabresd_6dq/BoardConfig.mk
+++ sabresd_6dq/BoardConfig.mk
@@ -6,5 +6,5 @@ include device/fsl/imx6/soc/imx6dq.mk
 include device/fsl/imx6/sabresd/SabreSDBoardConfigComm.mk
 include device/fsl-proprietary/gpu-viv/fsl-gpu.mk
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 fbmem=10M vmalloc=400M androidboot.console=ttymxc0
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M vmalloc=400M androidboot.console=ttymxc0
 TARGET_BOOTLOADER_CONFIG := 6q:mx6q_sabresd_android_config 6dl:mx6dl_sabresd_android_config
