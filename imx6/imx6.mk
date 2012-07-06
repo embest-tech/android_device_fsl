90f5f86ec95677963edcaa372739e09c12640d9c
Richard Liu <r66033@freescale.com>
2012-07-06 15:56:07 +0800

    ENGR00215859 fix mx6sl Gallery preview picture flicker issue
    fix mx6sl Gallery preview picture flicker issue
    separate HAVE_FSL_IMX_GPU to HAVE_FSL_IMX_GPU3D and HAVE_FSL_IMX_GPU2D
    
    Signed-off-by: Richard Liu <r66033@freescale.com>

--- imx6/imx6.mk
+++ imx6/imx6.mk
@@ -13,6 +13,7 @@ PRODUCT_PACKAGES += \
 	LiveWallpapersPicker			\
 	MagicSmokeWallpapers			\
 	Gallery2				\
+	Gallery		    			\
 	SoundRecorder				\
         Camera                                  \
 	FSLOta					\
