53e6e252a79b261c45a4f600e77b9c6bcb5ca2e8
b02247 <b02247@freescale.com>
2012-09-14 14:07:40 +0800

    ENGR00224187 imx6sl keypad ENTER does not work
    1. add idc file for imx-keypad, then keypad layout will be loaded.
    2. move the imx-keypad.idc, imx-keypad.kl to common/input
    
    Signed-off-by: b02247 <b02247@freescale.com>

--- imx6/evk_6sl.mk
+++ imx6/evk_6sl.mk
@@ -12,7 +12,8 @@ PRODUCT_COPY_FILES += \
 	device/fsl/evk_6sl/required_hardware.xml:system/etc/permissions/required_hardware.xml \
 	device/fsl/evk_6sl/vold.fstab:system/etc/vold.fstab \
 	device/fsl/evk_6sl/init.rc:root/init.freescale.rc \
-	device/fsl/evk_6sl/mxckpd.kl:system/usr/keylayout/mxckpd.kl \
+	device/fsl/common/input/imx-keypad.idc:system/usr/idc/imx-keypad.idc \
+	device/fsl/common/input/imx-keypad.kl:system/usr/keylayout/imx-keypad.kl \
 	device/fsl/evk_6sl/audio_policy.conf:system/etc/audio_policy.conf \
 	device/fsl/evk_6sl/audio_effects.conf:system/vendor/etc/audio_effects.conf
 
