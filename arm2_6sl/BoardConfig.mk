4a8b3b0e8161ac0ce99e77075b3fe30d757fc768
guoyin.chen <guoyin.chen@freescale.com>
2012-09-19 15:48:44 +0800

    ENGR00224965 Update fsl git for jb bring up
    Pass androidboot.hardware into boot parameter to make init
    load init.freescale.rc
    Disable WIFI build, which will be enabled after new wifi sdk integration
    Disable the imx tinyalsa and hwc, which will be enabled after refined
    Change the /dev/graphics/galcore permission, which need to be
    accessed by app for hardware render
    
    Change-Id: I13206a5e2ba80c946981e7241b55b3162eab4b71
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- arm2_6sl/BoardConfig.mk
+++ arm2_6sl/BoardConfig.mk
@@ -10,5 +10,5 @@ USE_CAMERA_STUB := true
 BOARD_HAVE_IMX_CAMERA := false
 PRODUCT_MODEL := ARM2-MX6SL
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init androidboot.console=ttymxc0 androidboot.hardware=freescale
 TARGET_BOOTLOADER_CONFIG := mx6sl:mx6sl_arm2_android_config
--- sabresd_6dq/BoardConfig.mk
+++ sabresd_6dq/BoardConfig.mk
@@ -6,12 +6,12 @@ include device/fsl/imx6/soc/imx6dq.mk
 include device/fsl/imx6/sabresd/SabreSDBoardConfigComm.mk
 include device/fsl-proprietary/gpu-viv/fsl-gpu.mk
 
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M vmalloc=400M androidboot.console=ttymxc0
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M vmalloc=400M androidboot.console=ttymxc0 androidboot.hardware=freescale
 
 
 ifeq ($(TARGET_USERIMAGES_USE_UBIFS),true)
 #UBI boot command line.
-BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M vmalloc=400M androidboot.console=ttymxc0  mtdparts=gpmi-nand:20m(bootloader),20m(bootimg),20m(recovery),-(root) gpmi_debug_init ubi.mtd=3
+BOARD_KERNEL_CMDLINE := console=ttymxc0,115200 init=/init video=mxcfb0 video=mxcfb1:off video=mxcfb2:off fbmem=10M vmalloc=400M androidboot.console=ttymxc0  mtdparts=gpmi-nand:20m(bootloader),20m(bootimg),20m(recovery),-(root) gpmi_debug_init ubi.mtd=3 androidboot.hardware=freescale 
 endif
 
 
