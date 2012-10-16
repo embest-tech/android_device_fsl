02b2c6dc00bada237fb40e9587667ab8f879e321
guoyin.chen <guoyin.chen@freescale.com>
2012-10-16 16:03:07 +0800

    ENGR00229800 Update board config to align Android JB build env
    Remove unused property setting in init.freescale.rc
    Update PRODUCT_AAPT_CONFIG for different screen support based on board config
    Update /system/etc/permisssion based on board's config
    Enable flag dalvik.gc.type-precise for dalvik
    Define PRODUCT_CHARACTERISTICS to be tablet
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- imx6/sabresd/SabreSDProductCommon.mk
+++ imx6/sabresd/SabreSDProductCommon.mk
@@ -7,3 +7,27 @@ PRODUCT_COPY_FILES += \
 # GPU files
 
 DEVICE_PACKAGE_OVERLAYS := device/fsl/imx6/sabresd/overlay
+
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_AAPT_CONFIG += large tvdpi hdpi
+
+PRODUCT_COPY_FILES += \
+	frameworks/native/data/etc/tablet_core_hardware.xml:system/etc/permissions/tablet_core_hardware.xml \
+	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
+	frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+	frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
+	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
+	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+
+# for PDK build, include only when the dir exists
+# too early to use $(TARGET_BUILD_PDK)
+ifneq ($(wildcard packages/wallpapers/LivePicker),)
+PRODUCT_COPY_FILES += \
+	packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+endif
