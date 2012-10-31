1e9b3bf4d37d2b3cd9dc0dd84c33d7f3266eda0e
b02247 <b02247@freescale.com>
2012-10-31 17:50:09 +0800

    ENGR00234076 bring up for imx6sl lcd display
    Add BoardConfig for imx6sl_evk
    
    Change-Id: I22dc65b314a3732d9f3c6f7e32fc23d4e38e8db0
    Signed-off-by: b02247 <b02247@freescale.com>

--- imx6/vendorsetup.sh
+++ imx6/vendorsetup.sh
@@ -7,3 +7,5 @@ add_lunch_combo sabreauto_6q-eng
 add_lunch_combo sabreauto_6q-user
 add_lunch_combo arm2_6sl-eng
 add_lunch_combo arm2_6sl-user
+add_lunch_combo evk_6sl-eng
+add_lunch_combo evk_6sl-user
