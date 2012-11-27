feeb1ba1c1c697833074a415caf10ba0fa656cef
b02247 <b02247@freescale.com>
2012-11-27 17:12:38 +0800

    ENGR00234970 testPackageSignatures failed in cts
    Add signature key for imx6/imx5x
    
    Change-Id: I7c5ed0064b12d6c976baa02fd767f056a4341c77
    Signed-off-by: b02247 <b02247@freescale.com>

--- imx6/imx6.mk
+++ imx6/imx6.mk
@@ -294,5 +294,8 @@ PRODUCT_TAGS += dalvik.gc.type-precise
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES := \
 	persist.sys.usb.config=mtp
 
+PRODUCT_DEFAULT_DEV_CERTIFICATE := \
+        device/fsl/common/security/testkey
+
 # include a google recommand heap config file.
 include frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk
