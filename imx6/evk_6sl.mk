c31153e3c7249b412390b9e29bf9cf7d2b32eb11
Xinyu Chen <xinyu.chen@freescale.com>
2012-12-26 18:04:33 +0800

    ENGR00238310 imx6: remove the separate board level configs
    Move all the imx6/arm2, sabresd, evk configs into
    the actual board configure directory.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- imx6/evk_6sl.mk
+++ imx6/evk_6sl.mk
@@ -7,9 +7,11 @@ $(call inherit-product, device/fsl/imx6/imx6.mk)
 PRODUCT_NAME := evk_6sl
 PRODUCT_DEVICE := evk_6sl
 
-include device/fsl/imx6/evk/ProductCommonEvk.mk
-
-PRODUCT_COPY_FILES +=	\
+PRODUCT_COPY_FILES += \
+	device/fsl/evk_6sl/required_hardware.xml:system/etc/permissions/required_hardware.xml \
+	device/fsl/evk_6sl/vold.fstab:system/etc/vold.fstab \
 	device/fsl/evk_6sl/init.rc:root/init.freescale.rc \
 	device/fsl/evk_6sl/mxckpd.kl:system/usr/keylayout/mxckpd.kl \
 	device/fsl/evk_6sl/audio_policy.conf:system/etc/audio_policy.conf
+
+DEVICE_PACKAGE_OVERLAYS := device/fsl/evk_6sl/overlay
