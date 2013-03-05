c9fcdefcc7130a3c2dca7cea64411cd866a663b8
Xiaowen Liu <b37945@freescale.com>
2013-03-05 14:30:51 +0800

    ENGR00215174 change camera features to pass testCameraFeatures.
    delete flash-autofocus feature.
    add flash feature.
    
    Signed-off-by: Xiaowen Liu <b37945@freescale.com>

--- imx6/sabreauto_6q.mk
+++ imx6/sabreauto_6q.mk
@@ -29,7 +29,6 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
 	frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
 	frameworks/native/data/etc/android.hardware.wifi.direct.xml:system/etc/permissions/android.hardware.wifi.direct.xml \
-	frameworks/native/data/etc/android.hardware.camera.front.xml:system/etc/permissions/android.hardware.camera.front.xml \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
