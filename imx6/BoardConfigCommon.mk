d6d57c8da2ba4ff5a3404efb50043e64440caad3
Xiaowen Liu <b37945@freescale.com>
2013-07-26 10:09:58 +0800

    ENGR00215174 add TARGET_CPU_VARIANT in imx6 and imx5 platform.
    add TARGET_CPU_VARIANT in imx6 and imx5 platform.
    
    Signed-off-by: Xiaowen Liu <b37945@freescale.com>

--- imx6/BoardConfigCommon.mk
+++ imx6/BoardConfigCommon.mk
@@ -7,6 +7,7 @@ TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 TARGET_NO_BOOTLOADER := true
