c5829ec993ea26c248e0653d52f6c206a9935484
Jianzheng Zhou <jianzheng.zhou@freescale.com>
2013-08-14 13:46:10 +0800

    ENGR00267092 BT: enable BLE feature
    Enable BLE feature. Test it using:
    1.AR3002 BT + sabreSD
    2.Bluetooth smart device supporting BLE: Polar H7 Heart Rate Sensor.
    3.Test apk:BluetoothLeGatt.apk
    
    Signed-off-by: Jianzheng Zhou <jianzheng.zhou@freescale.com>

--- imx6/sabresd_6dq.mk
+++ imx6/sabresd_6dq.mk
@@ -36,7 +36,8 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml \
 	frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
-	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml
+	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+	frameworks/native/data/etc/android.hardware.bluetooth_le.xml:system/etc/permissions/android.hardware.bluetooth_le.xml
 
 # for PDK build, include only when the dir exists
 # too early to use $(TARGET_BUILD_PDK)
