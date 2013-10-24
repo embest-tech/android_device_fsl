735fd7af2fabdc45f05a4459ae3eb2a400cd20d8
guoyin.chen <guoyin.chen@freescale.com>
2013-10-24 17:42:47 +0800

    ENGR00242365 Update BUILD_ID for jb4.3 ga rc2
    Update BUILD_ID as "1.0.0-rc2"
    Update BUILD_NUMBER as "20131108" to align new release date
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- hdmidongle_6dq/build_id.mk
+++ hdmidongle_6dq/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.0-rc1
-export BUILD_NUMBER=20131029
+export BUILD_ID=1.0.0-rc2
+export BUILD_NUMBER=20131108
