638d1504fdf1f0b1eda39f49675a1d55f5c1cc7d
Ke Qinghua <qinghua.ke@freescale.com>
2014-03-07 11:46:00 +0800

    ENGR00302221-2:CTS test faild for security setting and test Vroot
    Add graphics_device dir/file access rules for sabreauto_6q
    
    Signed-off-by: Ke Qinghua <qinghua.ke@freescale.com>

--- sabreauto_6q/BoardConfig.mk
+++ sabreauto_6q/BoardConfig.mk
@@ -103,3 +103,8 @@ endif
 endif
 
 TARGET_BOOTLOADER_CONFIG := mx6q:mx6q_sabreauto_android_config mx6dl:mx6dl_sabreauto_android_config mx6solo:mx6solo_sabreauto_android_config mx6q-nand:mx6q_sabreauto_nand_android_config mx6dl-nand:mx6dl_sabreauto_nand_android_config mx6solo-nand:mx6solo_sabreauto_nand_android_config
+BOARD_SEPOLICY_DIRS := \
+       device/fsl/sabreauto_6dq/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+       app.te
