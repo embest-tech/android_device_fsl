2a887c731a38b2d4240abb3a08210c45ef67430b
guoyin.chen <guoyin.chen@freescale.com>
2014-05-14 09:02:14 +0800

    ENGR00242365 Update BUILD_ID for kk4.4.2_1.0.0 GA rc1
    Update BUILD_ID for 1.0.0_rc1
    Update BUILD_NUMBER to ga release date
    
    Signed-off-by: guoyin.chen <guoyin.chen@freescale.com>

--- hdmidongle_6dq/build_id.mk
+++ hdmidongle_6dq/build_id.mk
@@ -18,5 +18,5 @@
 # (like "CRB01").  It must be a single word, and is
 # capitalized by convention.
 
-export BUILD_ID=1.0.0-beta-rc4
-export BUILD_NUMBER=20140418
+export BUILD_ID=1.0.0-rc1
+export BUILD_NUMBER=20140623
